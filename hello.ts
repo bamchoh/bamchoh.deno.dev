399a4940c17b4915a96c432c2014625714745f8a
Yoshihiko Yamanaka <bamchoh@gmail.com>
2021-03-30 20:56:32 +0900

    Add face_icon

--- hello.ts
+++ hello.ts
@@ -1,6 +1,10 @@
 addEventListener("fetch", (event) => {
   event.respondWith(
-    new Response("<h1>Hello world!</h1>", {
+    new Response(`
+    <h1>Hello world!</h1>
+    <h2>I'm bamchoh</h2>
+    <img src="https://github.com/bamchoh/bamchoh.deno.dev/blob/main/face_icon.jpg?raw=true" />
+    `, {
       status: 200,
       headers: {
         server: "denosr",
