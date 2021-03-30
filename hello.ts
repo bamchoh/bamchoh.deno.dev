dd824b699190e0a3da7d1de6cfc2e839e4b3405d
Yoshihiko Yamanaka <bamchoh@gmail.com>
2021-03-31 00:27:24 +0900

    commit

--- hello.ts
+++ hello.ts
@@ -1,15 +1,10 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Response(`
-    <h1>Hello world!</h1>
-    <h2>I'm bamchoh</h2>
-    <img src="https://github.com/bamchoh/bamchoh.deno.dev/blob/main/face_icon.jpg?raw=true" />
-    `, {
-      status: 200,
-      headers: {
-        server: "denosr",
-        "content-type": "text/html",
-      },
-    })
-  );
+import { serve } from "https://deno.land/x/sift/mod.ts";
+// You delare a route and its corresponding handler that returns a Response.
+serve({
+  "/": () => new Response("hello world"),
+  "/blog/:slug": (request, {slug}) => {
+      const post = `Hello, you visited ${slug}!`;
+      return new Response(post)
+  },
+  404: () => new Response("not found")
 });
